77f6f387e10f7c91a899a20c2ec6394da728d162
rupalpatil67 <rupalpatil1617@gmail.com>
2023-10-25 11:12:29 +0530

    upload to git

---
+++ src/main/java/Dockr/Assignment3/DockerAssign3Application.java
@@ -0,0 +1,13 @@
+package Dockr.Assignment3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerAssign3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerAssign3Application.class, args);
+	}
+
+}
